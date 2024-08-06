b49062cf0d121c21e236410af18b67ce31a95853
sophie <m.n.sophie@outlook.com>
2024-08-06 21:17:13 +0300

    Complete update

--- src/components/dexterity/AccountInfo.tsx
+++ src/components/dexterity/AccountInfo.tsx
@@ -5,60 +5,68 @@ import Button from "../Button";
 import { timeSince } from "utils/util";
 
 export const AccountInfo: FC = () => {
-    const { selectedProduct } = useProduct()
-    const {
-        trader,
-        cashBalance,
-        setCashBalance,
-        portfolioValue,
-        setPortfolioValue,
-        updated,
-        setUpdated,
-        lastUpdated,
-        setLastUpdated,
-        setOrderData,
-        orderData
-    } = useTrader()
+	const { selectedProduct } = useProduct();
+	const {
+		trader,
+		cashBalance,
+		setCashBalance,
+		portfolioValue,
+		setPortfolioValue,
+		updated,
+		setUpdated,
+		lastUpdated,
+		setLastUpdated,
+		setOrderData,
+		orderData,
+	} = useTrader();
 
-    const updateAccountInfo = useCallback(async () => {
-        if (!trader) return;
+	const updateAccountInfo = useCallback(async () => {
+		if (!trader) return;
+		const cashBalance = trader.getCashBalance().toDecimal();
+		const portfolioValue = trader.getPortfolioValue().toDecimal();
+		const orderData = Array.from(
+			await Promise.all(trader.getOpenOrders([selectedProduct.name])),
+		);
 
-        // Fetch & Update Trader Account information
+		setCashBalance(cashBalance);
+		setPortfolioValue(portfolioValue);
+		setOrderData(orderData);
 
-    }, [trader, selectedProduct]); // Removed markPrice and indexPrice
+		setUpdated(true);
+		setLastUpdated(Date.now());
+	}, [trader, selectedProduct]); // Removed markPrice and indexPrice
 
-    useEffect(() => {
+	useEffect(() => {
+		trader.connect(updateAccountInfo, updateAccountInfo);
 
-        // Stream Trader Account Information
+		return () => {
+			trader.disconnect();
+		};
+	}, [updateAccountInfo]);
 
-    }, [updateAccountInfo]);
+	return (
+		<>
+			<div className="border border-white rounded-lg p-4">
+				<h1 className="text-2xl mb-4">Account Info</h1>
+				<div className="grid grid-cols-2 gap-4">
+					<div className="font-semibold">Cash Balance:</div>
+					<div>${updated && cashBalance.toLocaleString()}</div>
 
-    return (
-        <>
-                <div className="border border-white rounded-lg p-4">
-                    <h1 className="text-2xl mb-4">Account Info</h1>
-                    <div className="grid grid-cols-2 gap-4">
-                        <div className="font-semibold">Cash Balance:</div>
-                        <div>${updated && cashBalance.toLocaleString()}</div>
+					<div className="font-semibold">Portfolio Value:</div>
+					<div>${updated && portfolioValue.toLocaleString()}</div>
 
-                        <div className="font-semibold">Portfolio Value:</div>
-                        <div>${updated && portfolioValue.toLocaleString()}</div>
+					<div className="font-semibold">Open Orders:</div>
+					<div>{orderData && (orderData.length ?? 0)}</div>
 
-                        <div className="font-semibold">Open Orders:</div>
-                        <div>{orderData && (orderData.length ?? 0)}</div>
-
-                        <div className="font-semibold">Last Updated:</div>
-                        <div>{updated && timeSince(lastUpdated)}</div>
-                    </div>
-                    <Button
-                        text={'↻'}
-                        onClick={updateAccountInfo}
-                        className="w-6 mt-4 bg-gradient-to-br from-[#80ff7d] to-[#80ff7d] hover:from-white hover:to-purple-300 text-black"
-                    />
-                </div>
-        </>
-    );
-}
-
-
-
+					<div className="font-semibold">Last Updated:</div>
+					<div>{updated && timeSince(lastUpdated)}</div>
+				</div>
+				<Button
+					text={"↻"}
+					onClick={updateAccountInfo}
+					className="w-6 mt-4 bg-gradient-to-br from-[#80ff7d] to-[#80ff7d] hover:from-white hover:to-purple-300 text-black"
+				/>
+			</div>
+		</>
+	);
+};
